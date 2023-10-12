c0f7b2fa83733f9b9bd527eed980576199887250
Monika <codingbymon@gmail.com>
2023-10-12 19:11:26 +0200

    added a div displaying when an animal is not found in the search engine

--- script.js
+++ script.js
@@ -1,17 +1,28 @@
 const input = document.querySelector('.search')
 const animal = document.querySelectorAll('.animal')
+const happyInfo = document.querySelector('.happy-info')
+
+const animalBox = document.querySelector('.animal-list')
 
 const searching = e => {
-    const text = e.target.value.toLowerCase()
-    console.log(text);
+	const text = e.target.value.toLowerCase()
 
-    animal.forEach(item => {
-        if(item.textContent.toLowerCase().indexOf(text) !== -1) {
-           item.style.display = 'flex'
-        } else {
-            item.style.display = 'none'
-        }
-    })
+	animal.forEach(item => {
+		if (item.textContent.toLowerCase().indexOf(text) !== -1) {
+			item.style.display = 'flex'
+		} else {
+			item.style.display = 'none'
+			addHappyInfo()
+		}
+	})
 }
 
 input.addEventListener('keyup', searching)
+
+const addHappyInfo = () => {
+	if (animalBox.offsetHeight < 150) {
+		happyInfo.style.display = 'flex'
+	} else if (animalBox.offsetHeight > 549) {
+		happyInfo.style.display = 'none'
+	}
+}
